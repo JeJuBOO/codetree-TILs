762d9f25adf0fd0618ad48a3bdf160ffc9120c9a
SK.BOO <71332005+JeJuBOO@users.noreply.github.com>
2023-12-14 21:31:14 +0900

    Title: 가장 큰 정사각형의 크기, Time: 230ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 231214/가장 큰 정사각형의 크기/size-of-the-largest-square.py
+++ 231214/가장 큰 정사각형의 크기/size-of-the-largest-square.py
@@ -4,8 +4,8 @@ from collections import deque
 N, M = map(int, sys.stdin.readline().split())
 board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-print(N,M)
-print(board)
+#print(N,M)
+#print(board)
 
 # 일단 작은 문제로 생각 오른쪽, 대각아래, 아래가 1이면 정사각형, 그리고 오른쪽,대각아래,아래에서 또 오른쪽,대각아래,아래가가 1이면 9
 queue = deque()
@@ -25,3 +25,4 @@ for i in range(N):
                 queue.append((x+1,y))
                 queue.append((x+1,y+1))
                 queue.append((x,y+1))
+print(max_size)
